db30c05b853d29929e4b81adedaf98add2effb32
Vinit Upadhyay <vinitupadhyay994@gmail.com>
2025-08-25 12:25:26 +0530

    Navigation bar

--- TradeAI/src/components/Navbar.jsx
+++ TradeAI/src/components/Navbar.jsx
@@ -2,20 +2,21 @@ import { Link } from "react-router-dom";
 
 const Navbar = ({ user, onLogout }) => {
   return (
-    <nav className="bg-black text-white flex justify-between items-center">
+    <nav className="bg-black text-white flex justify-between items-center p-1">
       <div className="flex items-center gap-3">
-        <img src="https://4kwallpapers.com/images/walls/thumbs_2t/13812.png" alt="" className="h-12"/>
-        <p className="text-2xl font-bold">Nextrade AI</p>
+        <p className="text-2xl font-bold px-5">Nextrade AI</p>
       </div>
-        <div className="">
+        <div className="flex flex-row items-center gap-9">
           <Link to="/" className="hover:text-gray-400">Home</Link>
           <Link to="/dashboard" className="hover:text-gray-400">Dashboard</Link>
+        </div>
+        <div className="">
           {!user ? (
             <Link to="/login" className="hover:text-gray-400">Login</Link>
           ) : (
             <button
               onClick={onLogout}
-              className="px-4 py-1 bg-red-500 rounded hover:bg-red-700"
+              className="px-4 py-1 m-1 cursor-pointer bg-gray-700 rounded-xl hover:bg-red-700"
             >
               Logout
             </button>
