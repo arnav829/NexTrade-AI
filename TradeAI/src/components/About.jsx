20280ffbbaf04b7d5f06f84993fc59cfcb6c55a7
Vinit Upadhyay <vinitupadhyay994@gmail.com>
2025-09-08 09:08:16 +0530

    changes in about page

--- TradeAI/src/components/About.jsx
+++ TradeAI/src/components/About.jsx
@@ -32,7 +32,7 @@ const About = () => {
         ].map((item, index) => (
           <div
             key={index}
-            className="bg-gray-800 rounded-2xl shadow-lg p-6 hover:scale-105 transition-transform duration-300"
+            className="bg-gray-800 rounded-2xl shadow-lg p-6 "
           >
             <h3 className="text-xl font-semibold text-blue-400 mb-3">
               {item.title}
